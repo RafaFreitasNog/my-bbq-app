09254ca8760659962208b3ed8a736ac41f145d9c
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-08-02 15:58:53 -0300

    Changed the element name to heroSection and finished it

--- src/components/imgAndTitleSection/index.jsx
+++ src/components/imgAndTitleSection/index.jsx
@@ -5,9 +5,12 @@ import './style.css'
 function ImgAndTitleSection() {
     return (
         <Fragment>
-            <div id="img-conteiner">
-                <div id="gradient-conteiner">
-                    
+            <div id="hero-img-conteiner">
+                <div id="hero-gradient-conteiner">
+                    <div id="hero-conteiner">
+                        <h1 id="hero-h1">MY BARBECUE APP</h1>
+                        <h4 id="hero-h4">YOUR ONLINE MEAT CALCULATOR</h4>
+                    </div>
                 </div>
             </div>
         </Fragment>
