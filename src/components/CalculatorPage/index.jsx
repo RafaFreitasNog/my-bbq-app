4b333992b15ba64ddecfc27a59990041c6d57870
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-08-07 20:49:50 -0300

    Started implementing the visuals for the calculator page. Changed the header height to 72px

--- src/components/CalculatorPage/index.jsx
+++ src/components/CalculatorPage/index.jsx
@@ -1,10 +1,17 @@
 import { Fragment } from 'react';
 import './style.css'
+// import backImg from '../../imgs/matt-connor-9Qs_9n2oSJo-unsplash.jpg'
 
 function CalculatorPage() {
     return ( 
         <Fragment>
-            <h1>Hello, World!</h1>
+            <div id='background-conteiner'>
+                <div id='grid-conteiner'>
+                    <div id='content-conteiner'>
+
+                    </div>
+                </div>
+            </div>
         </Fragment>
      );
 }
