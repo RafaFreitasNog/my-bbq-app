c46fc2536e6b1fc336be68e74f449d011c1914c0
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-08-09 16:28:36 -0300

    Fixed some issues with the hero's background image responsiveness

--- src/components/CalculatorPage/index.jsx
+++ src/components/CalculatorPage/index.jsx
@@ -1,24 +1,45 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import './style.css'
 // import backImg from '../../imgs/matt-connor-9Qs_9n2oSJo-unsplash.jpg'
 import arrowLeft from '../../imgs/arrow-left-white.png'
 import arrowRight from '../../imgs/arrow-right-white.png'
 
 function CalculatorPage() {
+
+    const [page, setPage] = useState(0);
+    const formTitles = ["Step 1", "Step 2", "Step 3"];
+
+    function handleNextClick() {
+        setPage((oldPage) => oldPage + 1)
+    }
+
+    function handlePrevClick() {
+        setPage((oldPage) => oldPage - 1)
+    }
+
     return ( 
         <Fragment>
             <div id='background-conteiner'>
                 <div id='grid-conteiner'>
                     <div id='content-conteiner'>
+
                         <div id='form-title'>
-                            <h6>oi</h6>
+                            <h4 id='title'>{formTitles[page]}</h4>
                         </div>
+
                         <div id='form-body'>
                             <h6>oi</h6>
                         </div>
+
                         <div id='form-buttons'>
-                            <button> <img src={arrowLeft} alt="arrow left" /> <h6 className='white'>previous</h6> </button>
-                            <button> <h6 className='white'>next</h6> <img src={arrowRight} alt="arrow right" /> </button>
+                            <button onClick={handlePrevClick} disabled={page == 0}>
+                                <img src={arrowLeft} alt="arrow left" />
+                                <h6 className='white'>previous</h6>
+                            </button>
+                            <button onClick={handleNextClick} disabled={page == formTitles.length - 1}>
+                                <h6 className='white'>next</h6>
+                                <img src={arrowRight} alt="arrow right" />
+                            </button>
                         </div>
                     </div>
                 </div>
