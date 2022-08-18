f8e72c2bd9db0ef7865a5d898279fd87538bac48
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-08-18 16:01:47 -0300

    Created new custom radio input that repllace the default ones

--- src/components/calculatorForms/formBarbecueAcomp/index.jsx
+++ src/components/calculatorForms/formBarbecueAcomp/index.jsx
@@ -20,13 +20,19 @@ function FormBarbecueAcomp(props) {
                 <p className='subtitle'>will there be any other food such as salads, rice, potatoes?</p>
                 <div id='radio-input-conteiner'>
                     <div className='radio-input'>
-                        <input type="radio" id='yes' name='acompanhamentos' value={true} checked={props.saveIsThereFood === "true"}
-                        onChange={(e) => handleRadioInputChange(e.target.value)}/>
+                        <label className='custom-button'>
+                            <input type="radio" id='yes' className='radio' name='acompanhamentos' value={true} checked={props.saveIsThereFood === "true"}
+                            onChange={(e) => handleRadioInputChange(e.target.value)}/>
+                            <div className={props.saveIsThereFood === "true" ? 'checkmark' : 'checkmark not-selected'}></div>
+                        </label>
                         <label htmlFor="yes" className='radio-input-label'>Yes</label>
                     </div>
                     <div className='radio-input second'>
-                        <input type="radio" id='no' name='acompanhamentos' value={false} checked={props.saveIsThereFood === "false"}
-                        onChange={(e) => handleRadioInputChange(e.target.value)}/>
+                        <label className='custom-button'>
+                            <input type="radio" id='no' className='radio' name='acompanhamentos' value={false} checked={props.saveIsThereFood === "false"}
+                            onChange={(e) => handleRadioInputChange(e.target.value)}/>
+                            <div className={props.saveIsThereFood === "false" ? 'checkmark' : 'checkmark not-selected'}></div>
+                        </label>
                         <label htmlFor="no" className='radio-input-label'>No</label>
                     </div>
                 </div>
